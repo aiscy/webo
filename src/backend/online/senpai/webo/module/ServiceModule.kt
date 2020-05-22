2a52fb3c9f977f9692bf0c5037ec2d96f9a0b016
Maxim Valeryevich Pavlov <maxpav137@gmail.com>
2020-05-22 15:05:31 +0500

    feat(WEBO-4): add the evolve service into the service module

--- src/backend/online/senpai/webo/module/ServiceModule.kt
+++ src/backend/online/senpai/webo/module/ServiceModule.kt
@@ -1,18 +1,16 @@
 package online.senpai.webo.module
 
-import com.github.jasync.sql.db.SuspendingConnection
-import com.github.jasync.sql.db.asSuspending
-import com.github.jasync.sql.db.mysql.MySQLConnectionBuilder
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
 import io.ktor.client.features.json.JacksonSerializer
 import io.ktor.client.features.json.JsonFeature
+import online.senpai.webo.service.EvolveService
+import online.senpai.webo.service.EvolveServiceImpl
 import online.senpai.webo.service.GithubApiV3
 import online.senpai.webo.service.GithubApiV3Impl
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import org.koin.experimental.builder.singleBy
-import java.util.concurrent.TimeUnit
 
 val serviceModule: Module = module(createdAtStart = true) {
     single<HttpClient> {
@@ -21,17 +19,5 @@ val serviceModule: Module = module(createdAtStart = true) {
         }
     }
     singleBy<GithubApiV3, GithubApiV3Impl>()
-    single<SuspendingConnection> {
-        MySQLConnectionBuilder.createConnectionPool {
-            username = "test"
-            host = "localhost"
-            port = 3306
-            password = "123456"
-            database = "test"
-            maxActiveConnections = 100
-            maxIdleTime = TimeUnit.MINUTES.toMillis(15)
-            maxPendingQueries = 10_000
-            connectionValidationInterval = TimeUnit.SECONDS.toMillis(30)
-        }.asSuspending
-    }
+    singleBy<EvolveService, EvolveServiceImpl>()
 }
