ad02b55cfe7d0232e686b34388cdcd598ca81fc7
Maxim Valeryevich Pavlov <maxpav137@gmail.com>
2020-05-22 15:11:40 +0500

    feat(WEBO-4): add the EvolveService interface and the implementation

---
+++ src/backend/online/senpai/webo/service/EvolveService.kt
@@ -0,0 +1,8 @@
+package online.senpai.webo.service
+
+import online.senpai.webo.model.EvolveModel
+
+interface EvolveService {
+    suspend fun characterLines(characterName: String): Iterable<EvolveModel>
+}
+
---
+++ src/backend/online/senpai/webo/service/EvolveServiceImpl.kt
@@ -0,0 +1,24 @@
+package online.senpai.webo.service
+
+import online.senpai.webo.domain.EvolveCharacterLine
+import online.senpai.webo.model.EvolveModel
+import online.senpai.webo.repository.EvolveRepository
+import org.koin.core.KoinComponent
+import org.koin.core.inject
+
+class EvolveServiceImpl : KoinComponent, EvolveService {
+    private val repository: EvolveRepository by inject()
+
+    override suspend fun characterLines(characterName: String): Iterable<EvolveModel> {
+        return repository
+            .listAll(characterName)
+            .mapIndexed { i: Int, characterLine: EvolveCharacterLine ->
+                EvolveModel(
+                    id = i,
+                    lineName = characterLine.lineName,
+                    lineText = characterLine.lineText,
+                    filePath = characterLine.filePath
+                )
+            }
+    }
+}
